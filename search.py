986e9b046274e622b78ed1f498fef1684f868b20
Matthew Moyo <87268111+moyom1337@users.noreply.github.com>
2025-05-28 16:53:49 +0100

    Update search.py
    Changed search.py to use search_player function

--- search.py
+++ search.py
@@ -5,11 +5,10 @@ import pandas as pd
 FPL_API_URL = "https://fantasy.premierleague.com/api/bootstrap-static/"
 data = requests.get(FPL_API_URL).json()
 
-# Make Dataframe
-players_df = pd.DataFrame(data["elements"])
-players_df = players_df[["id", "first_name", "second_name", "now_cost", "total_points"]]
-player_name = "Salah"
+def search_player(name_query):
+    players_df = pd.DataFrame(data["elements"])
+    players_df["full_name"] = players_df["first_name"] + " " + players_df["second_name"]
+    matching = players_df[players_df["second_name"].str.contains(name_query, case=False)]
+    return matching[["id", "full_name", "now_cost", "total_points"]]
 
-# Search for players that have names containing player_name
-matching = players_df[players_df["second_name"].str.contains(player_name, case=False)]
-print(matching)
+print(search_player("Mb"))
